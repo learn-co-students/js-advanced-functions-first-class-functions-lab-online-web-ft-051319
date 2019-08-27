5e51edcba445096d5d718306b79192689931504f
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-27 15:55:50 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+  const fn = function(drivers){
+    return drivers.slice(0,2)
+  }
+  return fn(drivers)
+}
+
+function returnLastTwoDrivers(drivers){
+  const fn = function(drivers){
+    return drivers.slice(drivers.length-2)
+  }
+  return fn(drivers)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+  return function(fare){
+    return multiplier*fare
+  }
+}
+
+function fareDoubler(fare){
+  return createFareMultiplier(2)(fare)
+}
+
+function fareTripler(fare){
+  return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers,fn){
+  return fn(drivers)
+}
