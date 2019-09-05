8f1f56a4302ca36f6a3f4af982d7eac694440ce8
Natalie <natsetc@gmail.com>
2019-09-04 18:03:24 -0700

    refactor notes

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const returnLastTwoDrivers = (drivers) => {
     return drivers.slice(-2);
 }
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(rate) {
-    return function multiplyFare(fare) {
+// function createFareMultiplier(rate) {
+//     return function multiplyFare(fare) {
+//         return rate * fare;
+//     }
+// }
+const createFareMultiplier = (rate) => {
+    return (fare) => {
         return rate * fare;
     }
 }
@@ -17,10 +22,12 @@ function createFareMultiplier(rate) {
 const fareDoubler = (fare, rate = 2) => {
     return createFareMultiplier(rate)(fare);
 }
+// const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = (fare, rate = 3) => {
     return createFareMultiplier(rate)(fare);
 }
+// const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(drivers, fxn) {
     return fxn(drivers);
